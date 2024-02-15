d9434bdfac1edfe74fe6848185f56af8491dfbd1
Mushrat Jahan <98585595+mushrat-jahan@users.noreply.github.com>
2024-02-15 18:38:35 +0600

    first commit

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/32589963-417d693a-ecf8-4443-9e59-3f463db6396b?access_key=${process.env.ACCESS_KEY}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
